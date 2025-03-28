605687b7f1f29f27aac992c2f6fe71e120cc8c66
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-03-27 21:35:12 -0300

    configuração inicial e add do tailwindcss para template layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavbarComponent } from "../@core/layout/side-navbar/side-navbar.component";
 
 @Component({
   selector: 'app-root',
